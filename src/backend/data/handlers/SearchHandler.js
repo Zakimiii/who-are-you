108573ce212b4d650c29eb6b146ea95267f6f4f7
Zakimiii <Th27876314@icloud.com>
2019-09-27 18:27:45 +0900

    re

---
+++ src/backend/data/handlers/SearchHandler.js
@@ -0,0 +1,9 @@
+import HandlerImpl from '@handlers/HandlerImpl';
+import { Set, Map, fromJS, List } from 'immutable';
+import models from '@models';
+
+export default class SearchHandler extends HandlerImpl {
+    constructor() {
+        super();
+    }
+}
