c5925cc95fb42fb47eadec6c44b249c61d6e8fd8
Zakimiii <Th27876314@icloud.com>
2019-09-27 20:00:17 +0900

    re

--- src/frontend/domain/repository/AnswerRepository.js
+++ src/frontend/domain/repository/AnswerRepository.js
@@ -1,9 +1,51 @@
 import RepositoryImpl from '@repository/RepositoryImpl';
 import { Set, Map, fromJS, List } from 'immutable';
 import { call, put, select, takeEvery } from 'redux-saga/effects';
+import safe2json from '@extension/safe2json';
+import data_config from '@constants/data_config';
 
 export default class AnswerRepository extends RepositoryImpl {
     constructor() {
         super();
+    }
+
+    async create(answer) {
+        const data = await super.apiCall('/api/v1/answer/create', {
+            answer: safe2json(answer),
+        });
+
+        return data;
+    }
+
+    async update(answer) {
+        const data = await super.apiCall('/api/v1/answer/update', {
+            answer: safe2json(answer),
+        });
+
+        return data;
+    }
+
+    async delete(answer) {
+        const data = await super.apiCall('/api/v1/answer/delete', {
+            answer: safe2json(answer),
+        });
+
+        return data;
+    }
+
+    async trash(answer) {
+        const data = await super.apiCall('/api/v1/answer/trash', {
+            answer: safe2json(answer),
+        });
+
+        return data;
+    }
+
+    async untrash(answer) {
+        const data = await super.apiCall('/api/v1/answer/trash', {
+            answer: safe2json(answer),
+        });
+
+        return data;
     }
 }
--- src/frontend/domain/repository/HeadingRepository.js
+++ src/frontend/domain/repository/HeadingRepository.js
@@ -1,63 +1,61 @@
 import RepositoryImpl from '@repository/RepositoryImpl';
 import { Set, Map, fromJS, List } from 'immutable';
 import { call, put, select, takeEvery } from 'redux-saga/effects';
+import safe2json from '@extension/safe2json';
+import data_config from '@constants/data_config';
 
 export default class HeadingRepository extends RepositoryImpl {
     constructor() {
         super();
     }
 
-    async getUser({ id, username }) {
-        const data = await super.apiCall('/api/v1/user', {
-            id,
-            username,
-        });
-        return data && data.user;
-    }
-
-    // async updateUser(user) {
-    //     const data = await super.apiCall('/api/v1/user/update', {
-    //         user,
-    //     });
-    //     return data;
-    // }
-
-    // async deleteUser(user) {
-    //     const data = await super.apiCall('/api/v1/user/delete', {
-    //         user,
-    //     });
-    //     return data;
-    // }
-
-    async getHeadings({ id, username, offset, limit, isMyAccount = false }) {
-        const data = await super.apiCall('/api/v1/user/headings', {
-            user_id: id,
-            username,
-            offset: Number(offset || 0),
-            limit: limit || data_config.fetch_data_limit('L'),
-            isMyAccount,
+    async create(heading) {
+        const data = await super.apiCall('/api/v1/heading/create', {
+            heading: safe2json(heading),
         });
 
-        return data && data.headings;
+        return data;
     }
 
-    async getAnswers({ id, username, offset, limit, isMyAccount = false }) {
-        const data = await super.apiCall('/api/v1/user/answers', {
-            user_id: id,
-            username,
+    async update(heading) {
+        const data = await super.apiCall('/api/v1/heading/update', {
+            heading: safe2json(heading),
+        });
+
+        return data;
+    }
+
+    async delete(heading) {
+        const data = await super.apiCall('/api/v1/heading/delete', {
+            heading: safe2json(heading),
+        });
+
+        return data;
+    }
+
+    async trash(heading) {
+        const data = await super.apiCall('/api/v1/heading/trash', {
+            heading: safe2json(heading),
+        });
+
+        return data;
+    }
+
+    async untrash(heading) {
+        const data = await super.apiCall('/api/v1/heading/trash', {
+            heading: safe2json(heading),
+        });
+
+        return data;
+    }
+
+    async getAnswers({ heading, offset, limit }) {
+        const data = await super.apiCall('/api/v1/heading/answers', {
+            heading_id: heading && heading.id,
+            limit: limit || data_config.fetch_data_limit('M'),
             offset: Number(offset || 0),
-            limit: limit || data_config.fetch_data_limit('L'),
-            isMyAccount,
         });
 
         return data && data.answers;
-    }
-
-    async syncNotificationId({ notification_id, current_user }) {
-        const data = await super.apiCall('/api/v1/user/notification_id/sync', {
-            notification_id,
-            user: safe2json(current_user),
-        });
-        return data && data.user;
     }
 }
