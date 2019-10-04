6d037e994aaab8fa7731f61b665a7684dd5bcdf5
Zakimiii <Th27876314@icloud.com>
2019-10-04 16:05:43 +0900

    set base layout

--- src/frontend/presentation/components/modules/SideBar/index.jsx
+++ src/frontend/presentation/components/modules/SideBar/index.jsx
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 import shouldComponentUpdate from '@extension/shouldComponentUpdate';
 import autobind from 'class-autobind';
 import tt from 'counterpart';
+import SideBarItem from '@elements/SideBarItem';
 
 class SideBar extends React.Component {
     static propTypes = {};
@@ -27,7 +28,59 @@ class SideBar extends React.Component {
     componentWillReceiveProps(nextProps) {}
 
     render() {
-        return <div className="side-bar" />;
+        const { section } = this.props;
+
+        const renderItem = items =>
+            items._enums.map((item, index) => {
+                switch (item.value) {
+                    case 'Border':
+                        return <div className="side-bar__border" key={index} />;
+                    case 'MyPage':
+                        if (!!current_user) {
+                            return (
+                                <li key={index} className="side-bar__item">
+                                    <SideBarItem
+                                        value={item.string()}
+                                        image={item.image}
+                                        link={item.link(current_user.id)}
+                                        active={item.active(
+                                            current_user.id,
+                                            pathname
+                                        )}
+                                    />
+                                </li>
+                            );
+                        } else {
+                            return (
+                                <li key={index} className="side-bar__item">
+                                    <SideBarItem
+                                        value={item.string()}
+                                        image={item.image}
+                                        onClick={showLogin}
+                                    />
+                                </li>
+                            );
+                        }
+                    default:
+                        return (
+                            <li key={index} className="side-bar__item">
+                                <SideBarItem
+                                    value={item.string()}
+                                    image={item.image}
+                                    link={item.link}
+                                    active={item.active(pathname)}
+                                />
+                            </li>
+                        );
+                }
+            });
+        return (
+            <div className="side-bar">
+                <ul className="side-bar__items">
+                    {section && renderItem(section)}
+                </ul>
+            </div>
+        );
     }
 }
 
