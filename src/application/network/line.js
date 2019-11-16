6eeb4739b0b3ecf6ea094a1685d1e54d7ebfef80
Zakimiii <Th27876314@icloud.com>
2019-11-16 15:06:53 +0900

    set base line handler

---
+++ src/application/network/line.js
@@ -0,0 +1,144 @@
+import querystring from 'querystring';
+import env from '@env/env.json';
+import fetch from 'isomorphic-fetch';
+import { ApiError } from '@extension/Error';
+import config from '@constants/config';
+import uuidv4 from 'uuid/v4';
+
+export default class LineHandler {
+    static getLinkToken = async () => {
+        const response = await fetch(
+            `https://api.line.me/v2/bot/user/${env.LINE.USER_ID}/linkToken`,
+            {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${env.LINE.ACCESS_TOKEN}`,
+                },
+            }
+        ).catch(e => {
+            throw new ClientError({
+                error: e,
+                tt_key: 'errors.invalid_response_from_server',
+            });
+        });
+
+        const contentType = response.headers.get('content-type');
+        if (!contentType || contentType.indexOf('application/json') === -1) {
+            throw new ApiError({
+                error: new Error('Invalid response from server'),
+                tt_key: 'errors.invalid_response_from_server',
+            });
+        }
+        let responseData = await response.json();
+        if (responseData.error) {
+            responseData.error.error = new Error(responseData.error.message);
+            throw new ApiError(responseData.error);
+        }
+
+        return responseData.linkToken;
+    };
+
+    static pushAccountLink = async linkToken => {
+        const response = await fetch(
+            `https://api.line.me/v2/bot/message/push`,
+            {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${env.LINE.ACCESS_TOKEN}`,
+                },
+                body: JSON.stringify({
+                    to: `${env.LINE.USER_ID}`,
+                    messages: [
+                        {
+                            type: 'template',
+                            altText: '連携まであと１タップ！',
+                            template: {
+                                type: 'buttons',
+                                text: '連携まであと１タップ！',
+                                actions: [
+                                    {
+                                        type: 'uri',
+                                        label: 'アカウントを連携',
+                                        uri: `${
+                                            config.CURRENT_APP_URL
+                                        }/line/link?linkToken=${linkToken}`,
+                                    },
+                                ],
+                            },
+                        },
+                    ],
+                }),
+            }
+        ).catch(e => {
+            throw new ClientError({
+                error: e,
+                tt_key: 'errors.invalid_response_from_server',
+            });
+        });
+
+        const contentType = response.headers.get('content-type');
+        if (!contentType || contentType.indexOf('application/json') === -1) {
+            throw new ApiError({
+                error: new Error('Invalid response from server'),
+                tt_key: 'errors.invalid_response_from_server',
+            });
+        }
+        let responseData = await response.json();
+        if (responseData.error) {
+            responseData.error.error = new Error(responseData.error.message);
+            throw new ApiError(responseData.error);
+        }
+    };
+
+    static redirectEndPointLinkUrl = linkToken =>
+        `https://access.line.me/dialog/bot/accountLink?linkToken=${
+            linkToken
+        }&nonce=${uuidv4()}`;
+}
+
+// const Line = function () {};
+
+// /**
+//  * LINE Notifyのトークンセット
+//  * @param {String} token LINE Notifyトークン
+//  */
+// Line.prototype.setToken = function(token) {
+//     this.token = token;
+// };
+
+// /**
+//  * LINE Notify実行
+//  * @param {String} text メッセージ
+//  */
+// Line.prototype.notify = function(text) {
+//   if(this.token == undefined || this.token == null){
+//     console.error('undefined token.');
+//     return;
+//   }
+//   console.log(`notify message : ${text}`);
+//   axios(
+//     {
+//       method: 'post',
+//       url: 'https://notify-api.line.me/api/notify',
+//       headers: {
+//         Authorization: `Bearer ${this.token}`,
+//         'Content-Type': 'application/x-www-form-urlencoded',
+//       },
+//       data: querystring.stringify({
+//         message: text,
+//       }),
+//     }
+//   )
+//   .then( function(res) {
+//     console.log(res.data);
+//   })
+//   .catch( function(err) {
+//     console.error(err);
+//   });
+// };
+
+// module.exports = Line;
---
+++ src/backend/webserver/api/lineMiddleware.js
@@ -0,0 +1,60 @@
+import { handleApiError } from '@extension/Error';
+import Gateway from '@network/gateway';
+import koa_router from 'koa-router';
+import koa_body from 'koa-body';
+import crypto from 'crypto';
+import models from '@models';
+import { esc, escAttrs } from '@models';
+import env from '@env/env.json';
+import coBody from 'co-body';
+import fetch from 'isomorphic-fetch';
+import LineHandler from '@network/line';
+
+export default function LineMiddleware(app) {
+    const router = koa_router({ prefix: '/line' });
+    app.use(router.routes());
+    const koaBody = koa_body({
+        formLimit: '5mb',
+        jsonLimit: '5mb',
+        textLimit: '5mb',
+    });
+
+    router.post('/webhook', koaBody, function*(ctx, next) {
+        console.log(this.request.body);
+    });
+
+    router.get('/link', koaBody, function*(ctx, next) {
+        const { linkToken } = this.params;
+        console.log(linkToken, this.request.body);
+    });
+
+    // router.get('/answer/:id/', koaBody, function*(ctx, next) {
+    //     let { id } = this.params;
+    //     id = Number(id.replace('.png', ''));
+    //     const answer = yield models.Answer.findOne({
+    //         where: {
+    //             id,
+    //         },
+    //     });
+
+    //     if (!answer || !answer.picture || answer.picture == '') {
+    //         const buffer = fs.readFileSync(
+    //             resolveAssetsPath(data_config.default_opg_image)
+    //         );
+    //         this.type = 'image/png';
+    //         this.response.type = 'image/png';
+    //         this.body = buffer;
+    //         return;
+    //     }
+
+    //     this.type = 'image/png';
+    //     this.response.type = 'image/png';
+    //     this.response.length = answer.picture.toString().length;
+    //     const buffer = yield getBase64ImageBuffer(
+    //         answer.picture.toString(),
+    //         id,
+    //         'answer'
+    //     );
+    //     this.body = buffer;
+    // });
+}
